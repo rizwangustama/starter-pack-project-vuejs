c0c89f0a12be5bf569d308202f79231c1ca37385
rizwan.gustama <rizwan.gustama@bayarind.id>
2025-05-20 20:40:29 +0700

    layout and alias vite

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div>
+    <header>
+      <h1>Dashboard</h1>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+    <footer>
+      <p>&copy; 2023 My Company</p>
+    </footer>
+  </div>
+</template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,40 @@
-import { createMemoryHistory, createRouter, type RouteRecordRaw } from 'vue-router';
+import { createWebHistory, createRouter, type RouteRecordRaw } from 'vue-router';
 
 // Define the routes for the application
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    component: () => import('../components/HelloWorld.vue'),
+    path: '/dashboard',
+    component: () => import('../layouts/DashboardLayout.vue'),
+    name: 'Dashboard',
+    children: [
+      {
+        path: '/',
+        name: 'Auth',
+        component: () => import('../components/Auth.vue'),
+      },
+      {
+        path: 'about',
+        name: 'DashboardAbout',
+        component: () => import('@/components/About.vue'),
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    name: 'LoginAuth',
+    component: () => import('../layouts/AuthLayout.vue'),
+    children: [
+      {
+        path: 'login',
+        name: 'Login',
+        component: () => import('../components/Auth.vue'),
+      }
+    ]
   }
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
