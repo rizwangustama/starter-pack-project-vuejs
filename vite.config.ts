748972ad4b0c136b86dd5ca0596a1a9245c6fc82
rizwan.gustama <rizwan.gustama@bayarind.id>
2025-05-21 11:59:16 +0700

    auto import and auto import component

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,36 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    visualizer({ open: true })
+    visualizer({ open: true }),
+    Components({
+      dirs: [
+        './src/shared/components'
+      ],
+      extensions: ['vue'],
+      deep: true,
+      dts: './src/app/components.d.ts',
+    }),
+    AutoImport(
+      {
+        imports: [ 'vue', 'vue-router', 'pinia' ],
+        dirs: [
+          './src/modules/**/stores',
+          './src/modules/**/types',
+          './src/modules/**/enums',
+        ],
+        dts: './src/app/auto-imports.d.ts',
+      }
+    )
   ],
-  // root: './src/app', // root application
+  // root application
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -23,5 +45,14 @@ export default defineConfig({
   build: {
     outDir: '../../dist',
     emptyOutDir: true
+  },
+  server: {
+    host: true,
+    port: 3000,
+    hmr: {
+      protocol: 'ws', // tetap ws karena Vite belum support hmr tanpa ws secara native
+      host: 'localhost',
+      port: 3000
+    }
   }
 })
